deee7f3a47cb7dbe01553e199fc0f3e063a2e43a
Daniel Conrad <dconradicle@gmail.com>
2020-09-22 19:12:38 -0700

    commented for better user experience

--- src/Compnents/playermenu/playerMenu.js
+++ src/Compnents/playermenu/playerMenu.js
@@ -14,20 +14,20 @@ const TopMenu = () => {
   <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="mr-auto">
       <Nav.Link><StyledLink to='/home'>Home</StyledLink></Nav.Link>
-      <Nav.Link><StyledLink to='/story'>Story</StyledLink></Nav.Link>
+      {/*<Nav.Link><StyledLink to='/story'>Story</StyledLink></Nav.Link>*/}
       <NavDropdown title="Characters" id="basic-nav-dropdown">
         <NavDropdown.Item><StyledLink to="characters#Ander Stormwind">Ander Stormwind</StyledLink></NavDropdown.Item>
         <NavDropdown.Item><StyledLink to="characters#Leon Feathertorn">Leon Feathertorn</StyledLink></NavDropdown.Item>
         <NavDropdown.Item><StyledLink to="characters#Kaius Sicarius">Kaius Sicarius</StyledLink></NavDropdown.Item>
         <NavDropdown.Item><StyledLink to="characters#Flacără 'Thicktail' Fierar">Flacără "Thicktail" Fierar</StyledLink></NavDropdown.Item>
       </NavDropdown>
-      <Nav.Link><StyledLink to='/history'>History</StyledLink></Nav.Link>
+      {/*<Nav.Link><StyledLink to='/history'>History</StyledLink></Nav.Link>*/}
       <Nav.Link><StyledLink to='/npc'>NPC</StyledLink></Nav.Link>
-      <NavDropdown title="Factions" id="basic-nav-dropdown">
+      {/*<NavDropdown title="Factions" id="basic-nav-dropdown">
         <NavDropdown.Item>Hu Port</NavDropdown.Item>
         <NavDropdown.Item>Angels Of Hell</NavDropdown.Item>
         <NavDropdown.Item>Sevova</NavDropdown.Item>
-      </NavDropdown>
+    </NavDropdown>
       <NavDropdown title="Gods" id="basic-nav-dropdown">
         <NavDropdown.Item >Theosius Feywild</NavDropdown.Item>
         <NavDropdown.Item >Nocturn Shadowfell</NavDropdown.Item>
@@ -38,7 +38,7 @@ const TopMenu = () => {
         <NavDropdown.Item ><StyledLink to="maps#World">World</StyledLink></NavDropdown.Item>
         <NavDropdown.Item >Nocturn</NavDropdown.Item>
         <NavDropdown.Item >HaliDom</NavDropdown.Item>
-      </NavDropdown>
+      </NavDropdown>*/}
     </Nav>
   </Navbar.Collapse>
 </StyledNavBar>
