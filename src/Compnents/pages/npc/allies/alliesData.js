41523b7da2cb1651ed33d3b653c1691d789cebe3
Daniel Conrad <dconradicle@gmail.com>
2020-09-22 17:38:43 -0700

    fixed indenting

--- src/Compnents/pages/npc/allies/alliesData.js
+++ src/Compnents/pages/npc/allies/alliesData.js
@@ -8,21 +8,21 @@ const AllieObj = [
         name: 'Hannah',
         picture: hanna,
         story:'A kind old Lady who sought out the adventurers to help with a rat infestation in her basement\n\
-        she was then the unofficial bouncer to the adventurers sewer lair where she would always offer tea and cake.\n\
-        Hannah looks at these new adventurers as her own children, checking in on them when they are missing or  gone astray\n\
-        She alwasy has something nice to say and always has stories to tell, she makes everyone feel as if they are the most important person to her\n\
-        Hannah doesnt talk about her past or her deceased husband much but has hinted that she used to be an adventure like the group until her husband died.\n\
-        Currently she is helping Pan Piper take care of refugees at the halidom. If she isnt working she is talking with Midgard.' 
+she was then the unofficial bouncer to the adventurers sewer lair where she would always offer tea and cake.\n\
+Hannah looks at these new adventurers as her own children, checking in on them when they are missing or  gone astray\n\
+She alwasy has something nice to say and always has stories to tell, she makes everyone feel as if they are the most important person to her\n\
+Hannah doesnt talk about her past or her deceased husband much but has hinted that she used to be an adventure like the group until her husband died.\n\
+Currently she is helping Pan Piper take care of refugees at the halidom. If she isnt working she is talking with Midgard.' 
     },
     {
         id:2,
         name: 'Kii, Smii, Sti, Mii',
         picture: kii,
         story:'Kii and Smii wanted to leave the bloodshed of underdark wars and start a new life taking care of a tavern\n\
-        When they followed the cave systems to the bright world they came across an abandoned settlement of 4 houses.\n\
-        They were busy making it into their dream inn when 4 odd looking goblins came out of the caves (the adventures), they took\
-        them to stay at the inn where they bargined for their stay. When the Halidom was discovered they quickly helped out around the place\
-        and when the refugies started showing up, they were eager to help the new customers. Hi people'
+When they followed the cave systems to the bright world they came across an abandoned settlement of 4 houses.\n\
+They were busy making it into their dream inn when 4 odd looking goblins came out of the caves (the adventures), they took\
+them to stay at the inn where they bargined for their stay. When the Halidom was discovered they quickly helped out around the place\
+and when the refugies started showing up, they were eager to help the new customers. Hi people'
 
     }
 
